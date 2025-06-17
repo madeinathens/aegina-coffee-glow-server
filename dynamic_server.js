245b0eef5e3de6855dce145dd94f7a851759397a
madeinathens.eth <136375392+madeinathens@users.noreply.github.com>
2025-06-17 10:20:43 +0300

    Update dynamic_server.js

--- dynamic_server.js
+++ dynamic_server.js
@@ -19,4 +19,4 @@ app.get("/metadata", (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(process.env.PORT || 3000, () => console.log(`Server running on port ${process.env.PORT || 3000}`));
